cc9ce4a6e62bf51d780d0065a8b259e7302c9cbc
coder-wings <rskatiyar1920@gmail.com>
2021-05-18 16:18:38 +0530

    functionality addition

--- services/loadData.js
+++ services/loadData.js
@@ -15,20 +15,42 @@ function loadData(url,getData){
 
 function getData(res){
     let data=JSON.parse(res.responseText);
-    console.log(res.responseText);
+   // console.log(res.responseText);
     let contentContainer=document.querySelector(".content-container");
-    data.forEach(function(element){
+    data.forEach(function(elem){
         let template=`<div class="card">
-        <img src=${element.product_image} alt=${element.product_name}/>
+        <img src=${elem.product_image} alt=${elem.product_name}/>
         <div class="card-title">
           <div class="card-body">
-            <h2>${element.product_name+" "+"Price: "+element.price}</h2>
-            <p>${element.description}</p>
-            <button>Add TO Cart</button>
-          </div>
+            <h2>${elem.product_name+" "+"Price: "+elem.price}</h2>
+            <p>${elem.description}</p>
+            </div>
+            <input type="hidden" value="${elem.product_id}"/>
+            <button class="cartitems" >Add TO Cart</button>
         </div>
       </div>`;
       contentContainer.innerHTML+=template;
     });
-
+    /*##############ADD TO CART BUTTON HANDLING##########*/
+   // localStorage.setItem("items","[]");
+   
+    let items=[]; 
+    localStorage.setItem("items",JSON.stringify(items));
+    let elements=document.getElementsByClassName("cartitems");
+    for(let i=0;i<elements.length;i++){
+        elements[i].addEventListener("click",handleElements);
+    }
+    function handleElements(){
+        let id=this.previousElementSibling.value;
+        let items=JSON.parse(localStorage.getItem("items"));
+        items.push(id);
+        localStorage.setItem("items",JSON.stringify(items));
+        document.getElementsByClassName("cart-items")[0].innerHTML=`${JSON.parse(localStorage.getItem("items")).length}`;
+    }
 }
+
+
+
+
+
+
